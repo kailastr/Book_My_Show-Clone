9e93b10e606660173397faafc0ccf0cba34f7b95
kailastr <mailtokailastr@gmail.com>
2023-10-21 23:04:17 +0530

    added modal to movie page

--- src/components/MovieHero/MovieInfo.Component.jsx
+++ src/components/MovieHero/MovieInfo.Component.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react'
 import { MovieContext } from '../../context/Movie.context';
+import PaymentModal from '../PaymentModal/Payment.Component';
 
 const MovieInfo = ({ movie }) => {
 
@@ -21,7 +22,7 @@ const MovieInfo = ({ movie }) => {
 
     return (
         <>
-            {/* < PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price} /> */}
+            < PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price} />
             <div className="flex flex-col gap-8 ml-10" >
                 <h1 className='text-white text-5xl font-bold'>
                     {movie.original_title}
@@ -33,10 +34,10 @@ const MovieInfo = ({ movie }) => {
                     <h4>{movie.runtime} min | {genres} </h4>
                 </div>
                 <div className="flex items-center gap-3 w-full">
-                    <button className="bg-red-500 w-full py-3 text-white font-semibold rounded-lg">
+                    <button onClick={rentMovie} className="bg-red-500 w-full py-3 text-white font-semibold rounded-lg">
                         Rent ₹ 299
                     </button>
-                    <button className="bg-red-700 w-full py-3 text-white font-semibold rounded-lg">
+                    <button onClick={buyMovie} className="bg-red-700 w-full py-3 text-white font-semibold rounded-lg">
                         Buy ₹ 499
                     </button>
                 </div>
---
+++ src/components/PaymentModal/Payment.Component.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+
+const PaymentModal = ({ setIsOpen, isOpen, price }) => {
+
+    const closeModal = () => {
+        setIsOpen(false);
+    }
+
+    return (
+        <>
+            <Transition appear show={isOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="text-lg font-medium leading-6 text-gray-900"
+                                    >
+                                        Please make a payment
+                                    </Dialog.Title>
+                                    <div className="mt-2">
+                                        <p className="text-sm text-gray-500">
+                                            Hello please click on the below button to make a payment
+                                        </p>
+                                    </div>
+
+                                    <div className="mt-4 flex gap-3">
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                                            onClick={closeModal}
+                                        >
+                                            Price {price}
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                                            onClick={closeModal}
+                                        >
+                                            Cancel Payment
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </>
+    )
+}
+
+export default PaymentModal
